49ea81116de47e03f9e4da9a3858efec5e967d2f
Felix Nehrke <felix.nehrke@di-unternehmer.com>
2020-01-17 17:43:08 +0100

    Expose most variables to the module-surface
    This surface of the module has much more power over the actual behavior
    through this change since it is now able to change parts of the
    configuration without changing the module.

--- codebuild.tf
+++ codebuild.tf
@@ -5,16 +5,16 @@ resource "aws_cloudwatch_log_group" "build_log_group" {
 resource "aws_codebuild_project" "build" {
   name          = var.repository_slug
   description   = "This project builds ${var.repository_name}${local.name_suffix}"
-  build_timeout = "30"
+  build_timeout = var.timeout
   service_role  = aws_iam_role.build_role.arn
 
   artifacts { type = "NO_ARTIFACTS" }
   cache { type = "NO_CACHE" }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:2.0"
-    type                        = "LINUX_CONTAINER"
+    compute_type                = var.compute_type
+    type                        = var.compute_environment_type
+    image                       = var.runtime_image
     image_pull_credentials_type = "CODEBUILD"
   }
 
@@ -29,13 +29,12 @@ resource "aws_codebuild_project" "build" {
 resource "aws_codebuild_webhook" "build_webhook" {
   project_name = aws_codebuild_project.build.name
   filter_group {
-    filter {
-      type    = "EVENT"
-      pattern = "PUSH"
-    }
-    filter {
-      type    = "HEAD_REF"
-      pattern = "master"
+    dynamic "filter" {
+      for_each = var.webhook_filter
+      content {
+        type    = filter.value.type
+        pattern = filter.value.pattern
+      }
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -22,3 +22,31 @@ variable "buildspec" {
   description = "The name of the buildspec file"
   default     = "buildspec.yml"
 }
+
+variable "timeout" {
+  description = "The timeout of the build"
+  default     = "30"
+}
+
+variable "compute_type" {
+  description = "The compute-type for the codebuild, see https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-compute-types.html"
+  default     = "BUILD_GENERAL1_SMALL"
+}
+
+variable "compute_environment_type" {
+  description = "The actual runtime-type of the compute_type, see: https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-compute-types.html"
+  default     = "LINUX_CONTAINER"
+}
+
+variable "runtime_image" {
+  description = "The runtime image used to build the code, see: https://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref-available.html"
+  default     = "aws/codebuild/standard:2.0"
+}
+
+variable "webhook_filter" {
+  description = "The filters which are applied onto the webhooks"
+  default     = [{
+    type    = "EVENT"
+    pattern = "PUSH"
+  }]
+}
